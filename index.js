309fa5eab7fd1a2ab3abf8c37789fbadef1769f2
Alber <albertorg0087@gmail.com>
2023-05-19 12:24:44 +0100

    GENERAL - Connect DB

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const express = require('express')
-const app = express()
-
 require('dotenv').config()
 
+const express = require('express')
+const sequelize = require('./db')
 
+
+
+const app = express()
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate()
+  } catch(err) {
+    console.error(err)
+    throw new Error('Cannot connect to the database')
+  }
+}
 
 const start = async () => {
   try {
     app.get('/', (req, res) => res.send('Welcome to the API!'))
     await app.listen(process.env.PORT)
+    await connectDB()
     console.info(`Server running on port ${process.env.PORT}`)
-  } catch (err) {
-    console.log(err)
+  } catch(err) {
+    console.error(err)
+    throw new Error(`Cannot start server on port ${process.env.PORT}`)
   }
 }
 
